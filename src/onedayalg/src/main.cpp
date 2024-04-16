0c8ad3751ca13e1c1e95dfd1270941fe10e3a8cd
woong137 <woong137137@gmail.com>
2024-04-17 03:34:51 +0900

    Reorder includes and fix formatting

--- src/onedayalg/src/main.cpp
+++ src/onedayalg/src/main.cpp
@@ -1,23 +1,24 @@
-#include <ros/ros.h>
-#include <onedayalg/onedayalg.h>
 #include <msgs/dataset_array_msg.h>
 #include <msgs/map_array_msg.h>
+#include <onedayalg/onedayalg.h>
+#include <ros/ros.h>
 
-int main (int argc, char **argv)
-{
-	ros::init(argc, argv, "supervisor");
-	ros::NodeHandle node("~");
 
-	ros::AsyncSpinner spinner(1);
-  	spinner.start();
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "supervisor");
+  ros::NodeHandle node("~");
 
-	Supervisor sup;
- 	 
-	sup.pub1 = node.advertise<msgs::dataset_array_msg>("/result",1);
-	
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
 
-	ros::Subscriber sub1 = node.subscribe("/map_data", 1,             &Supervisor::callback_map, &sup);
-	ros::Subscriber sub2 = node.subscribe("/history", 1,             &Supervisor::callback_dataset, &sup);
-	 
-	ros::waitForShutdown();
+  Supervisor sup;
+
+  sup.pub1 = node.advertise<msgs::dataset_array_msg>("/result", 1);
+
+  ros::Subscriber sub1 =
+      node.subscribe("/map_data", 1, &Supervisor::callback_map, &sup);
+  ros::Subscriber sub2 =
+      node.subscribe("/history", 1, &Supervisor::callback_dataset, &sup);
+
+  ros::waitForShutdown();
 }
